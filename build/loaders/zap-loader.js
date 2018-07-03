e2cec79c3b75a199b3e8a6fa79abdb871ed30a4e
dscrobonia <davidscrobonia@gmail.com>
2018-07-02 21:29:21 -0700

    adding webpack, zap-loader, and xo linter

---
+++ build/loaders/zap-loader.js
@@ -0,0 +1,10 @@
+module.exports = function zapLoader(source) {
+  source = source.replace("<<ZAP_HUD_FILES>>", "https://zap");
+  source = source.replace("<<ZAP_HUD_URL>>", "https://targetdomain.com");
+  source = source.replace("<<ZAP_HUD_API>>", "https://zap/api");
+  source = source.replace("<<ZAP_HUD_WS>>", "https://zap/websocket");
+  source = source.replace("<<ZAP_HUD_TOOLS>>", "'http://zap/to/some/tool'");
+  source = source.replace("<<ZAP_SHARED_SECRET>>", "sometestsecret");
+
+  return source;
+};
