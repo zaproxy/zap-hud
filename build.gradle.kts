0d26715d7507557e7ce81cccbfa38f0ad42c9212
thc202 <thc202@gmail.com>
2018-12-17 15:54:31 +0000

    Prepare next dev iteration
    Change version back to 0.2.0.
    Update changelog and ZapAddOn.xml file.

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,7 +20,7 @@ repositories {
 }
 
 status = "alpha"
-version = "0.1.2"
+version = "0.2.0"
 
 val genHudFilesDir = layout.buildDirectory.dir("genHudFiles").get()
 val generatedI18nJsFileDir = genHudFilesDir.dir("i18nJs")
