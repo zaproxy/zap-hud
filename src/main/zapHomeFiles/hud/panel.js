ac90ac0fade0d8ee30c80853ad74f8a9de07e94a
David Scrobonia <dscrobonia@users.noreply.github.com>
2019-02-17 12:22:49 -0800

    add transitions to buttons (#414)

--- src/main/zapHomeFiles/hud/panel.js
+++ src/main/zapHomeFiles/hud/panel.js
@@ -28,12 +28,15 @@ Vue.component('hud-button', {
 			currentData: this.data,
 			currentIcon: this.icon,
 			showData: true,
-			showLabel: false,
 			orientation: orientation,
 			marginleft: '0rem',
 			marginright: '0rem',
+			labelmarginleft: '0rem',
+			labelmarginright: '0rem',
 			isActive: false,
-			isDisabled: false
+			isDisabled: false,
+			isClosed: true,
+			direction: 'ltr'
 		}
 	},
 	computed: {
@@ -58,14 +61,33 @@ Vue.component('hud-button', {
 			navigator.serviceWorker.controller.postMessage({action: "buttonMenuClicked", tool: this.name, frameId: frameId, tabId: tabId});
 		},
 		mouseOver() {
-			this.showLabel = true;
+			this.labelmarginleft = this.marginleft;
+			this.labelmarginright = this.marginright;
 			this.isActive = true;
+			this.isClosed = false;
 			expandPanel();
 		},
 		mouseLeave() {
-			this.showLabel = false;
+			this.labelmarginleft = '0rem';
+			this.labelmarginright = '0rem';
 			this.isActive = false;
-			contractPanel();
+		},
+		transitionEnd() {
+			let areAllButtonsClosed = true;
+
+			if (!this.isActive) {
+				this.isClosed = true;
+			}
+
+			this.$parent.$children.forEach(child => {
+				if (!child.isClosed) {
+					areAllButtonsClosed = false;
+				}
+			})
+
+			if (areAllButtonsClosed) {
+				contractPanel();
+			}
 		}
 	},
 	created() {
@@ -74,9 +96,11 @@ Vue.component('hud-button', {
 		// set the margins depending on the orientation
 		if (orientation === 'left') {
 			self.marginleft = '.5rem';
+			self.direction = "ltr"
 		}
 		else {
 			self.marginright = '.5rem'
+			self.direction = "rtl"
 		}
 
 		eventBus.$on('updateButton', data => {
