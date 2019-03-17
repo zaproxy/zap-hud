ed7270d5b39fb6f50b0ecd319ae57e35a8d4936d
jaywon <jason@sudokrew.com>
2019-03-16 15:31:05 -1000

    Convert custom environment parsing syntax for Post HTML syntax for variable substition

---
+++ src/main/zapHomeFiles/hud/posthtml.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  plugins: {
+    "posthtml-expressions": {
+      locals: {
+        ZAP_HUD_FILES: process.env.ZAP_HUD_FILES,
+        ZAP_HUD_URL: process.env.ZAP_HUD_URL,
+        ZAP_HUD_API: process.env.ZAP_HUD_API,
+        ZAP_HUD_WS: process.env.ZAP_HUD_WS,
+        ZAP_HUD_TOOLS: process.env.ZAP_HUD_TOOLS,
+        ZAP_SHARED_SECRET: process.env.ZAP_SHARED_SECRET
+      }
+    }
+  }
+};
