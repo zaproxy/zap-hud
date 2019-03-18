3c72d1d768bd52895c7ebc3dbc68ebc1ddc90f23
jaywon <jason@sudokrew.com>
2019-03-18 10:41:01 -1000

    :heavy_plus_sign: Add Display components for migration/refactor

---
+++ src/main/zapHomeFiles/hud/entrypoints/Display/Display.vue
@@ -0,0 +1,31 @@
+<template>
+  
+</template>
+
+
+<script>
+  export default {
+    
+  }
+</script>
+
+<style lang="scss" scoped>
+  @import '../../scss/variables.scss';
+
+  body {
+    background-color: transparent;
+  }
+
+  .site-tree-node {
+    list-style: none;
+    margin-left: 0;
+    padding-left: 1em;
+    text-indent: -1em;
+  }
+
+  span.errorMessages {
+    color: $red;
+  }
+
+</style>
+
