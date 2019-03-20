7d4c1a86a968d67147d4a5e13d3ed765653504ed
jaywon <jason@sudokrew.com>
2019-03-19 20:41:28 -1000

    :recycle: Break out Tab and Tabs to their own components

---
+++ src/main/zapHomeFiles/hud/components/Tab.vue
@@ -0,0 +1,35 @@
+<template id="tab-template">
+  <div v-show="isActive">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  template: "#tab-template",
+  props: {
+    name: { required: true },
+    selected: { default: false },
+    disabled: { default: false }
+  },
+  data() {
+    return {
+      isActive: false,
+      isDisabled: false
+    };
+  },
+  computed: {
+    href() {
+      return "#" + this.name.toLowerCase().replace(/ /g, "-");
+    }
+  },
+  mounted() {
+    this.isActive = this.selected;
+    this.isDisabled = this.disabled;
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
+
