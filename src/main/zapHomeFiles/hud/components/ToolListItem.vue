1ba51e7bc1d0acdc8d89b0db3df5a8e885181b2a
jaywon <jason@sudokrew.com>
2019-03-19 20:43:56 -1000

    :recycle: Break out ToolListItem to its own component

---
+++ src/main/zapHomeFiles/hud/components/ToolListItem.vue
@@ -0,0 +1,29 @@
+<!-- select tool li component -->
+<template id="tool-li-template">
+    <li class="menu-item" @click="toolSelect()">
+        <a href="#">
+            <img :src="image" /> {{ label }}
+        </a>
+    </li>
+</template>
+
+
+<script>
+export default {
+	template: '#tool-li-template',
+	props:['image', 'label', 'toolname', 'port'],
+	methods: {
+		close: function() {
+			this.$emit('close');
+		},
+		toolSelect: function() {
+			this.port.postMessage({'action': 'toolSelected', 'toolname': this.toolname})
+			this.close();
+		}
+	}
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
